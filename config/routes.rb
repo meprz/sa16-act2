66ea05fb6314db97dbf5041dc75a062c89ac32c1
Miguel Perez <migeperezjr@gmail.com>
2024-04-16 01:06:08 -0500

    made home page, made navbar for all pages, set up routes for other pages

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root 'pages#home'
+  get 'home', to: 'pages#home', as: 'home'
+  get 'about', to: 'pages#about', as: 'about'
+  get 'my_work', to: 'pages#my_work', as: 'my_work'
+  get 'contact', to: 'pages#contact', as: 'contact'
 end
